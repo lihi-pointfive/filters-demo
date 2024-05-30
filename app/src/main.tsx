b34ddeef0bcf799c9962b123a534f6ed36069de1
Lihi <lihi.shamriz@pointfive.co>
2024-05-30 13:25:07 +0300

    Bug fix

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.render(
     <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
-    </React.StrictMode>
-);
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    rootElement,
+  );
+} else {
+  console.error("Root element not found");
+}
